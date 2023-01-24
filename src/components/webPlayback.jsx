c04f8216c09b130ea6f3a716c4e637160f6ae719
Tim Quigg <timjquigg@gmail.com>
2023-01-24 10:39:18 -0700

    Update WebPlayback component to use a custom hook, useWebplayer

--- src/components/webPlayback.jsx
+++ src/components/webPlayback.jsx
@@ -1,18 +1,7 @@
-import { useState, useEffect } from "react";
-
-const track = {
-  name: "",
-  album: {
-    images: [{ url: "" }],
-  },
-  artists: [{ name: "" }],
-};
+import useWebplayer from "../hooks/useWebplayer.js";
 
 export default function WebPlayback(props) {
-  const [player, setPlayer] = useState(undefined);
-  const [isPaused, setIsPaused] = useState(false);
-  const [isActive, setIsActive] = useState(false);
-  const [currentTrack, setCurrentTrack] = useState(track);
+  const { player, currentTrack, isPaused, name } = useWebplayer(props.token);
 
   const handleClick = (event) => {
     player.togglePlay().then(() => {
@@ -20,54 +9,11 @@ export default function WebPlayback(props) {
     });
   };
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://sdk.scdn.co/spotify-player.js";
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    window.onSpotifyWebPlaybackSDKReady = () => {
-      const player = new window.Spotify.Player({
-        name: "Web Playback SDK",
-        getOAuthToken: (cb) => {
-          cb(props.token);
-        },
-        volume: 0.5,
-      });
-
-      setPlayer(player);
-
-      player.addListener("ready", ({ device_id }) => {
-        console.log("Ready with Device ID", device_id);
-      });
-
-      player.addListener("not_ready", ({ device_id }) => {
-        console.log("Device ID has gone offline", device_id);
-      });
-
-      player.addListener("player_state_changed", (state) => {
-        if (!state) {
-          return;
-        }
-
-        setCurrentTrack(state.track_window.current_track);
-        setIsPaused(state.paused);
-
-        console.log("player in player state", player);
-        player.getCurrentState().then((state) => {
-          !state ? setIsActive(false) : setIsActive(true);
-        });
-      });
-
-      player.connect();
-    };
-  }, [props.token]);
-
   return (
     <>
       <div className="container">
         <div className="main-wrapper">
+          <p>{name}</p>
           <img
             src={currentTrack.album.images[0].url}
             className="now-playing__cover"
@@ -90,7 +36,7 @@ export default function WebPlayback(props) {
             &lt;&lt;
           </button>
 
-          <button className="btn-spotify" onClick={handleClick}>
+          <button className="btn-spotify" onClick={() => player.togglePlay()}>
             {isPaused ? "PLAY" : "PAUSE"}
           </button>
 
---
+++ src/hooks/useWebplayer.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+
+const track = {
+  name: "",
+  album: {
+    images: [{ url: "" }],
+  },
+  artists: [{ name: "" }],
+};
+
+export default function useWebplayer(token) {
+  const [player, setPlayer] = useState(undefined);
+  const [isPaused, setIsPaused] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [currentTrack, setCurrentTrack] = useState(track);
+  const name = "Tim's Web Player";
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new window.Spotify.Player({
+        name: name,
+        getOAuthToken: (cb) => {
+          cb(token);
+        },
+        volume: 0.5,
+      });
+
+      setPlayer(player);
+
+      player.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+      });
+
+      player.addListener("not_ready", ({ device_id }) => {
+        console.log("Device ID has gone offline", device_id);
+      });
+
+      player.addListener("player_state_changed", (state) => {
+        if (!state) {
+          return;
+        }
+
+        setCurrentTrack(state.track_window.current_track);
+        setIsPaused(state.paused);
+
+        player.getCurrentState().then((state) => {
+          !state ? setIsActive(false) : setIsActive(true);
+        });
+      });
+
+      player.connect();
+    };
+  }, [token]);
+
+  return { player, currentTrack, isPaused, name };
+}
